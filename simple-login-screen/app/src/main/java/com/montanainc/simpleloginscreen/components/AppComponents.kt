ba4bfde9dcf408ee7cc739e0bbcb5923cedb2f56
DevHumbleChris <dev.humblechris254@gmail.com>
2023-11-24 15:43:03 +0300

    added checkbox component

--- simple-login-screen/app/src/main/java/com/montanainc/simpleloginscreen/components/AppComponents.kt
+++ simple-login-screen/app/src/main/java/com/montanainc/simpleloginscreen/components/AppComponents.kt
@@ -1,12 +1,15 @@
 package com.montanainc.simpleloginscreen.components
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,7 +22,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -133,7 +138,8 @@ fun PasswordTextFieldComponent(labelValue: String, icon: ImageVector) {
             )
         },
         trailingIcon = {
-            val iconImage = if (isPasswordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
+            val iconImage =
+                if (isPasswordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
             val description = if (isPasswordVisible) "Show Password" else "Hide Password"
             IconButton(onClick = {
                 isPasswordVisible = !isPasswordVisible
@@ -144,4 +150,26 @@ fun PasswordTextFieldComponent(labelValue: String, icon: ImageVector) {
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
         visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
     )
+}
+
+@Composable
+fun CheckboxComponent() {
+    var isChecked by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(56.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = {
+                isChecked = it
+            }
+        )
+        NormalTextComponent(value = "By continuing you accept our")
+    }
 }
--- simple-login-screen/app/src/main/java/com/montanainc/simpleloginscreen/screens/SignupScreen.kt
+++ simple-login-screen/app/src/main/java/com/montanainc/simpleloginscreen/screens/SignupScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.montanainc.simpleloginscreen.components.CheckboxComponent
 import com.montanainc.simpleloginscreen.components.HeadingTextComponent
 import com.montanainc.simpleloginscreen.components.MyTextFieldComponent
 import com.montanainc.simpleloginscreen.components.NormalTextComponent
@@ -54,6 +55,7 @@ fun SignupScreen() {
                     labelValue = "Password",
                     icon = Icons.Outlined.Lock
                 )
+                CheckboxComponent()
             }
         }
     }
